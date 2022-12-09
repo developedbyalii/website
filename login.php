2ab0f8706c6247afd00e94f0657a2571d314e15f
developedbyalii <97248259+developedbyalii@users.noreply.github.com>
2022-12-09 15:42:45 -0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="An impressive and flawless site template that includes various UI elements and countless features, attractive ready-made blocks and rich pages, basically everything you need to create a unique and professional website.">
+  <meta name="keywords" content="bootstrap 5, business, corporate, creative, gulp, marketing, minimal, modern, multipurpose, one page, responsive, saas, sass, seo, startup, html5 template, site template">
+  <meta name="author" content="elemis">
+  <title>PLACEHOLDER -</title>
+  <?php include('inc/styles.php') ?>
+</head>
+
+<body>
+  <div class="content-wrapper">
+  
+   
+    <!-- /header -->
+    <section class="wrapper image-wrapper bg-image bg-overlay bg-overlay-light-600 text-white" >
+      <div class="container pt-17 pb-10 pt-md-10 pb-md-21 text-center">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <img src="./assets/img/logo-dark.png" class="img-fluid logo-img" alt="" width="300px">
+            <!-- <h1 class="display-1 mb-3">Sign In</h1>
+            <nav class="d-inline-block" aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Sign In</li>
+              </ol>
+            </nav> -->
+            <!-- /nav -->
+          </div>
+          <!-- /column -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container -->
+    </section>
+    <!-- /section -->
+    <section class="wrapper bg-light">
+      <div class="container pb-14 pb-md-16">
+        <div class="row">
+          <div class="col-lg-7 col-xl-6 col-xxl-5 mx-auto mt-n20">
+            <div class="card">
+              <div class="card-body p-11 text-center">
+                <h2 class="mb-3 text-start">Welcome Back</h2>
+                <p class="lead mb-6 text-start">Fill your email and password to sign in.</p>
+                <form class="text-start mb-3">
+                  <div class="form-floating mb-4">
+                    <input type="email" class="form-control" placeholder="Email" id="loginEmail">
+                    <label for="loginEmail">Email</label>
+                  </div>
+                  <div class="form-floating password-field mb-4">
+                    <input type="password" class="form-control" placeholder="Password" id="loginPassword">
+                    <span class="password-toggle"><i class="uil uil-eye"></i></span>
+                    <label for="loginPassword">Password</label>
+                  </div>
+                  <a class="btn btn-primary rounded-pill btn-login w-100 mb-2">Sign In</a>
+                </form>
+                <!-- /form -->
+                <p class="mb-1"><a href="#" class="hover">Forgot Password?</a></p>
+                <p class="mb-0">Don't have an account? <a href="./signup.php" class="hover">Sign up</a></p>
+                <div class="divider-icon my-4">or</div>
+                <nav class="nav social justify-content-center text-center">
+                  <a href="#" class="btn btn-circle btn-sm btn-google"><i class="uil uil-google"></i></a>
+                  <a href="#" class="btn btn-circle btn-sm btn-facebook-f"><i class="uil uil-facebook-f"></i></a>
+                  <a href="#" class="btn btn-circle btn-sm btn-twitter"><i class="uil uil-twitter"></i></a>
+                </nav>
+                <!--/.social -->
+              </div>
+              <!--/.card-body -->
+            </div>
+            <!--/.card -->
+          </div>
+          <!-- /column -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container -->
+    </section>
+    <!-- /section -->
+  </div>
+  <!-- /.content-wrapper -->
+  
+  <div class="progress-wrap">
+    <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+      <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+    </svg>
+  </div>
+  <?php include('inc/scripts.php') ?>
+</body>
+
+</html>
